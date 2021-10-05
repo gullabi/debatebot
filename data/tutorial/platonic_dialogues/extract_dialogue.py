8d11aba86604ce8ff000627a86b85d425e40c422
gullabi <bkulebi@gmail.com>
2021-10-05 18:14:25 +0200

    new data segmentation added to platonic ipynb

--- data/tutorial/platonic_dialogues/extract_dialogue.py
+++ data/tutorial/platonic_dialogues/extract_dialogue.py
@@ -2,6 +2,8 @@ import re
 import os
 import json
 
+from nltk.tokenize import sent_tokenize
+
 DOT = re.compile('(?<=\s)([A-Z][A-Za-z]+)\.([\S\s]*?)(?=\s\s[A-Z][A-Za-z]+\.)')
 COLON = re.compile('(?<=\s)([A-Z][A-Za-z]+):([\S\s]*?)(?=\s\s[A-Z][A-Za-z]+:)')
 
@@ -18,6 +20,43 @@ def main():
 
     with open('platonic.json', 'w') as out:
         json.dump(dialogues, out, indent = 2)
+def join_sentences(sentences, length=200):
+  new_sentences = []
+  current_length = 0
+  current_no = 0
+  current_sentence = ''
+  for sentence in sentences:
+    current_length += len(sentence)
+    current_no += 1
+    if current_length > length:
+      if current_sentence:
+        new_sentences.append(current_sentence)
+        current_length = 0
+        current_no = 0
+        current_sentence = ''
+      else:
+        #print('WARNING: sentence too long:%i'%len(sentence))
+        current_sentence = ' '.join((current_sentence, sentence[:200]))
+    else:
+      current_sentence = ' '.join((current_sentence, sentence))
+    new_sentences.append(current_sentence)
+  return new_sentences
+
+def tokenize(dialogues):
+  new_dialogues = []
+  for speaker, speech in dialogues:
+    if len(speech) > 200:
+      sentences = sent_tokenize(speech)
+      joined_sentences = join_sentences(sentences)
+      print(len(sentences), len(joined_sentences))      
+      #print(speaker, speech)
+      print(sentences)
+      print(joined_sentences)
+      for sentence in joined_sentences:
+        new_dialogues.append((speaker, sentence))
+    else:
+        new_dialogues.append((speaker, speech))
+  return new_dialogues
 
 def get_dialogues(filename):
     content = open(filename).read()
